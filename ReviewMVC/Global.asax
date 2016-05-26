24e27bec8b089fa6e1eb38698f89a99740a5b688
your name <you@example.com>
2016-05-26 18:22:21 -0400

    Initial commit.

---
+++ ReviewMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ReviewMVC.MvcApplication" Language="C#" %>
---
+++ ReviewMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ReviewMVC.Startup))]
+namespace ReviewMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
